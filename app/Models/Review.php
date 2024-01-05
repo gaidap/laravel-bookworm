b9a34f3673c5ac032220e03b3bcf53be9c2a3e9a
Paul Gaida <pc.gaida@gmail.com>
2024-01-05 14:33:20 +0100

    Add fillable attributes to Review model
    The 'rating' and 'comment' attributes have been included in the $fillable array of the Review model. This change will enable mass assignment on these attributes, allowing for more convenient and efficient creation or update of Review instances. Additionally, factory classes and seeder modifications were implemented to facilitate testing and database operations.

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,6 +9,8 @@ class Review extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['rating', 'comment'];
+
     public function book()
     {
         return $this->belongsTo(Book::class);
