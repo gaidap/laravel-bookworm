3b385496caec95a135927c0dd4a22ff2f4680f63
Paul Gaida <pc.gaida@gmail.com>
2024-03-05 21:34:38 +0100

    Add title scope to Book model
    The scopeTitle method has been added to the Book model. This additional feature will allow for easy querying of books based on their title. Also, return type declarations have been added for better code understanding and maintainability.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,15 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
     use HasFactory;
 
-    public function reviews()
+    public function reviews(): HasMany
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'like', '%' . $title . '%');
     }
 }
