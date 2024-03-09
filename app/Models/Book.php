6870729bc9d2707477aada60c664cef0042d45eb
Paul Gaida <pc.gaida@gmail.com>
2024-03-09 19:02:42 +0100

    Enhance Book model with new query scopes
    The Book model is now equipped with new querying scopes: 'popular', 'highest rated', and 'unpopular'. These additions contribute to a more refined and efficient data extraction based on book popularity, their ratings, or the lack thereof.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -21,18 +22,46 @@ class Book extends Model
         return $query->where('title', 'like', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query): Builder
+    public function scopePopular(Builder $query, ?string $from = null, ?string $to = null): Builder|QueryBuilder
     {
-        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+        return $query->withCount([
+            'reviews' => fn(Builder $query) => $this->dateRangeFilter($query, $from, $to),
+        ])->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query): Builder
+    public function scopeHighestRated(Builder $query, ?string $from = null, ?string $to = null): Builder
     {
-        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
+        return $query->withAvg([
+            'reviews' => fn(Builder $query) => $this->dateRangeFilter($query, $from, $to),
+        ], 'rating')->orderBy('reviews_avg_rating', 'desc');
     }
 
-    public function scopeUnpopular(Builder $query): Builder
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder
     {
-        return $query->withCount('reviews')->orderBy('reviews_count');
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    public function scopeUnpopular(Builder $query, ?string $from = null, ?string $to = null): Builder
+    {
+        return $query->withCount([
+            'reviews' => fn(Builder $query) => $this->dateRangeFilter($query, $from, $to),
+        ])->orderBy('reviews_count');
+    }
+
+    private function dateRangeFilter(Builder $query, ?string $from = null, ?string $to = null): Builder
+    {
+        if ($from && !$to) {
+            return $query->where('created_at', '>=', $from);
+        }
+
+        if (!$from && $to) {
+            return $query->where('created_at', '<=', $to);
+        }
+
+        if ($from && $to) {
+            return $query->whereBetween('created_at', [$from, $to]);
+        }
+
+        return $query;
     }
 }
